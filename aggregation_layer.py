1444b9bb5e2387755cadd63122e4eac10382fe18
johan <jsoltysik98@gmail.com>
2021-03-17 17:00:47 +0100

    One liner agregation_layer

--- aggregation_layer.py
+++ aggregation_layer.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+
+
 class Reduction_Layer(torch.nn.Module):
     """ Custom Linear layer but mimics a standard linear layer """
     def __init__(self, size_in, size_out):
@@ -12,10 +14,10 @@ class Reduction_Layer(torch.nn.Module):
         # torch.nn.init.uniform_(self.A, -y, y)
 
     def forward(self, x):
-        W = torch.nn.Sigmoid()(self.A) # Applying sigmoid to weight
-        tensors = []
-        for elem in W.t():
-          max = torch.max(x * elem, 1)[0]
-          tensors.append(max)
-        output = torch.stack(tensors, 1)
-        return output
+        W = torch.nn.Sigmoid()(self.A)  # Applying sigmoid to weight
+        # tensors = []
+        # for elem in W.t():
+        #  max = torch.max(x * elem, 1)[0]
+        #  tensors.append(max)
+        # output = torch.stack(tensors, 1)
+        return (W.t() * x.unsqueeze(1)).max(dim=-1)[0]

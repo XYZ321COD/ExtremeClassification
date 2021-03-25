d4e0ace44d1c0fbe323b48d3db02b29ca1ebe574
johan <jsoltysik98@gmail.com>
2021-03-25 18:58:09 +0100

    Remove test config and use generators instead

--- tests/test_aggregation_layer.py
+++ tests/test_aggregation_layer.py
@@ -6,6 +6,21 @@ import torch
 from utils.aggregation_layer import aggregate
 
 
+def generate_two_random_dimensions(*, number_of_pairs: int = 10):
+    """
+    Generates two integers representing
+    input and output size of aggregation layer.
+    """
+    return (torch.randint(0, 1000, size=(2,)).tolist() for _ in range(number_of_pairs))
+
+
+def generate_dimension_for_identity(*, number_of_pairs: int = 10):
+    """
+    Generates dimension of square aggregation matrix.
+    """
+    return torch.randint(0, 1000, size=(number_of_pairs,)).tolist()
+
+
 @ddt.ddt
 class AggregationTest(unittest.TestCase):
     """
@@ -16,7 +31,8 @@ class AggregationTest(unittest.TestCase):
     def setUp(self) -> None:
         self.batch_size: int = 128
 
-    @ddt.file_data('test_config.yaml')
+    @ddt.idata(generate_two_random_dimensions())
+    @ddt.unpack
     def test_output_size(self, size_in: int, size_out: int) -> None:
         """
         Tests if calculated output size is properly calculated.
@@ -27,17 +43,14 @@ class AggregationTest(unittest.TestCase):
         out = aggregate(W, x)
         self.assertEqual(out.size(), torch.Size([self.batch_size, size_out]))
 
-    @ddt.file_data('test_config.yaml')
-    def test_aggregation_for_identity(self, size_in: int, size_out: int) -> None:
+    @ddt.idata(generate_dimension_for_identity())
+    def test_aggregation_for_identity(self, dim: int) -> None:
         """
         Tests aggregation for W matrix equal to identity.
         Output should be equal input tensor.
         """
-        if size_in != size_out:
-            self.skipTest("Test only if W is identity matrix")
-
-        W = torch.eye(size_in, requires_grad=False)
-        x = torch.rand(self.batch_size, size_out, requires_grad=False)
+        W = torch.eye(dim, requires_grad=False)
+        x = torch.rand(self.batch_size, dim, requires_grad=False)
         out = aggregate(W, x)
         self.assertTrue(torch.allclose(x, out))
 

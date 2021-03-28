cd48551c60d0efe1beef8e11a0f322a8b02d466b
johan <jsoltysik98@gmail.com>
2021-03-28 18:29:23 +0200

    New aggregation methods

--- tests/test_aggregation_layer.py
+++ tests/test_aggregation_layer.py
@@ -1,9 +1,10 @@
+import abc
 import unittest
 
 import ddt
 import torch
 
-from utils.aggregation_layer import aggregate
+from utils.aggregation_layer import max_aggregate, prod_aggregate
 
 
 def generate_two_random_dimensions(*, number_of_pairs: int = 10):
@@ -22,15 +23,11 @@ def generate_dimension_for_identity(*, number_of_pairs: int = 10):
 
 
 @ddt.ddt
-class AggregationTest(unittest.TestCase):
+class AggregationTest(abc.ABC):
     """
-    Tests which checks if aggregation layer is
+    Abstract test scheme which checks if aggregation layer is
     implemented correctly.
     """
-
-    def setUp(self) -> None:
-        self.batch_size: int = 128
-
     @ddt.idata(generate_two_random_dimensions())
     @ddt.unpack
     def test_output_size(self, size_in: int, size_out: int) -> None:
@@ -40,7 +37,7 @@ class AggregationTest(unittest.TestCase):
         W = torch.rand(size_in, size_out, requires_grad=False)
         x = torch.rand(self.batch_size, size_in, requires_grad=False)
 
-        out = aggregate(W, x)
+        out = self.aggregate(W, x)
         self.assertEqual(out.size(), torch.Size([self.batch_size, size_out]))
 
     @ddt.idata(generate_dimension_for_identity())
@@ -51,8 +48,26 @@ class AggregationTest(unittest.TestCase):
         """
         W = torch.eye(dim, requires_grad=False)
         x = torch.rand(self.batch_size, dim, requires_grad=False)
-        out = aggregate(W, x)
+        out = self.aggregate(W, x)
         self.assertTrue(torch.allclose(x, out))
+
+
+class MaxAggregationTests(AggregationTest, unittest.TestCase):
+    """
+    Performs aggregation tests for max operator.
+    """
+    def setUp(self) -> None:
+        self.batch_size = 128
+        self.aggregate = max_aggregate
+
+
+class ProdAggregationTests(AggregationTest, unittest.TestCase):
+    """
+    Performs aggregation tests for product of probabilities.
+    """
+    def setUp(self) -> None:
+        self.batch_size = 128
+        self.aggregate = prod_aggregate
 
 
 if __name__ == '__main__':
--- utils/aggregation_layer.py
+++ utils/aggregation_layer.py
@@ -1,35 +1,57 @@
-import numpy as np
+import abc
+
 import torch
 
 
-def aggregate(W: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+def max_aggregate(W: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
     """
-    Performs aggregation for given weights and input tensors.
+    Performs aggregation using max operator for given weights and input tensors.
     """
-    seq = []
-    for i in range(len(x)):
-        sub_seq = []
-        for j in range(W.size()[0]):
-            sub_seq.append(W[j, :] * x[i, j])
-
-        z = torch.stack(sub_seq, dim=1)
-        z = z.max(dim=1)[0]
-        seq.append(z)
-
-    out = torch.stack(seq, 0)
-    return out
+    return (W * x.unsqueeze(dim=-1)).max(dim=1)[0]
 
 
-class Reduction_Layer(torch.nn.Module):
-    """ Custom Linear layer but mimics a standard linear layer """
-    def __init__(self, size_in, size_out):
+def prod_aggregate(W: torch.Tensor, x: torch.Tensor, *, alpha: float = 1.0) -> torch.Tensor:
+    """
+    Performs aggregation using product of probabilities for given weights and input tensors.
+    """
+    out = 1.0 - torch.pow((W * x.unsqueeze(dim=-1)), alpha)
+    return torch.pow(1.0 - out.prod(dim=1), 1 / alpha)
+
+
+class ReductionLayer(abc.ABC, torch.nn.Module):
+    """
+    Abstract custom reduction layer class containing logic
+    for weight initialization.
+    """
+
+    def __init__(self, size_in: int, size_out: int) -> None:
         super().__init__()
         self.size_in, self.size_out = size_in, size_out
         A = torch.full((size_in, size_out), -1000)
         A = A.fill_diagonal_(1000).to(torch.float32)
-        self.A = torch.nn.Parameter(A)  # nn.Parameter is a Tensor that's a module parameter.
+        self.A = torch.nn.Parameter(A)
 
-    def forward(self, x):
-        W = torch.nn.Sigmoid()(self.A)  # Applying sigmoid to weight
-        return aggregate(W, x)
-        # return (W * x.unsqueeze(1)).max(dim=-1)[0]
+
+class MaxReductionLayer(ReductionLayer):
+    """
+    Custom layer which performs max aggregation of weights.
+    """
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        W = torch.sigmoid(self.A)
+        return max_aggregate(W, x)
+
+
+class ProdReductionLayer(ReductionLayer):
+    """
+    Custom layer which performs aggregation using product
+    of probabilities.
+    """
+
+    def __init__(self, size_in: int, size_out: int, *, alpha: float = 1.0) -> None:
+        super().__init__(size_in, size_out)
+        self.alpha = alpha
+
+    def forward(self, x) -> torch.Tensor:
+        W = torch.sigmoid(self.A)
+        return prod_aggregate(W, x, alpha=self.alpha)

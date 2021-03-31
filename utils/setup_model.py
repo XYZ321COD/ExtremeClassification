e30d793d6775c0c614737d2d0b6dd872fe055894
johan <jsoltysik98@gmail.com>
2021-03-31 20:50:14 +0200

    Aggregation method from config

--- utils/setup_model.py
+++ utils/setup_model.py
@@ -1,14 +1,17 @@
 from utils.models import LaNet
 import yaml
 import torch.nn as nn
-from utils.aggregation_layer import Reduction_Layer
+from utils.aggregation_layer import MaxReductionLayer, ProdReductionLayer
 name_to_model = {"LaNet": LaNet.get_LaNet}
 
 file = open(r'config.yaml')
 cfg = yaml.load(file, Loader=yaml.FullLoader)
 
+
 def add_aggregation_to_model(model, input_size, output_size):
-    return nn.Sequential(model, Reduction_Layer(input_size, output_size))
+    reduction_layer = MaxReductionLayer if cfg['hyperparameters']['aggregation_method'] else ProdReductionLayer
+    return nn.Sequential(model, reduction_layer(input_size, output_size))
+
 
 def get_model(trial):
     reduction_value = trial.suggest_int("reduction_value", min(cfg['hyperparameters']['reduction_value']), max(cfg['hyperparameters']['reduction_value']))
@@ -18,4 +21,3 @@ def get_model(trial):
         return add_aggregation_to_model(model, reduction_value, num_of_classes), reduction_value
 
     return name_to_model[cfg['options']['model']](num_of_classes), reduction_value
-
